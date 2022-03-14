63d747e18ac5e69ccc0d39952a4ab3c61400c5fd
Eleanor Chamberlain-Stoltzfus <eleanor.stoltzfus@gmail.com>
2022-03-14 17:00:45 +0000

    completed api testing

--- spec/api_testing_spec.rb
+++ spec/api_testing_spec.rb
@@ -2,21 +2,10 @@ require 'api_testing'
 
 RSpec.describe TimeError do
   it 'returns the difference in seconds' do
-    time_error = TimeError.new
-    from_time = Time.new(2022, 03, 14, 14, 10, 04)
-    to_time = Time.new(2022, 03, 14, 16, 10, 04)
-    allow(time_error).to receive(:get_server_time).and_return(to_time)
-    allow(Time).to receive(:now).and_return(from_time)
-    expect(time_error.error).to eq 7200.0
-  end
-
-  it 'returns the server time' do
-    time_error = TimeError.new
-    requester_dbl = double :requester
-    from_time = Time.new(2022, 03, 14, 14, 10, 04)
-    to_time = Time.new(2022, 03, 14, 16, 10, 04)
-    allow(Time).to receive(:now).and_return(from_time)
-    allow(requester_dbl).to receive(:get).with(URI("https://worldtimeapi.org/api/ip")).and_return('{"abbreviation":"GMT","client_ip":"2.25.179.217","datetime":"2022-03-14T16:41:36.214527+00:00","day_of_week":1,"day_of_year":73,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":0,"timezone":"Europe/London","unixtime":1647276096,"utc_datetime":"2022-03-14T16:41:36.214527+00:00","utc_offset":"+00:00","week_number":11}')
-    expect(time_error.error).to eq 9385.478979
+    fake_requester = double :requester
+    allow(fake_requester).to receive(:get).with(URI("https://worldtimeapi.org/api/ip")).and_return('{"abbreviation":"GMT","client_ip":"2.25.179.217","datetime":"2022-03-14T16:41:36.214527+00:00","day_of_week":1,"day_of_year":73,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":0,"timezone":"Europe/London","unixtime":1647276096,"utc_datetime":"2022-03-14T16:41:36.214527+00:00","utc_offset":"+00:00","week_number":11}')
+    time = Time.new(2022, 03, 14, 14, 10, 04)
+    time_error = TimeError.new(fake_requester)
+    expect(time_error.error(time)).to eq 9092.214527
   end
 end
