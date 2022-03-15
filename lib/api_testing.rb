212062eea987d58b33276cbce9679c6ce22dc9ce
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-03-15 11:03:47 +0000

    kernel

--- lib/api_testing.rb
+++ lib/api_testing.rb
@@ -3,6 +3,9 @@ require 'json'
 require 'net/http'
 
 class TimeError
+  def initialize(requester)
+    @requester = requester
+  end
   # Returns difference in seconds between server time
   # and the time on this computer
   def error
@@ -12,7 +15,7 @@ class TimeError
   private
 
   def get_server_time
-    text_response = Net::HTTP.get(URI("https://worldtimeapi.org/api/ip"))
+    text_response = @requester.get(URI("https://worldtimeapi.org/api/ip"))
     json = JSON.parse(text_response)
     return DateTime.parse(json["utc_datetime"]).to_time
   end
--- spec/api_testing_spec.rb
+++ spec/api_testing_spec.rb
@@ -2,7 +2,8 @@ require 'api_testing'
 
 RSpec.describe TimeError do
   it 'returns the difference in seconds' do
-    time_error = TimeError.new
+    requester_dbl = double :requester
+    time_error = TimeError.new(requester_dbl)
     from_time = Time.new(2022, 03, 14, 14, 10, 04)
     to_time = Time.new(2022, 03, 14, 16, 10, 04)
     allow(time_error).to receive(:get_server_time).and_return(to_time)
@@ -11,12 +12,12 @@ RSpec.describe TimeError do
   end
 
   it 'returns the server time' do
-    time_error = TimeError.new
     requester_dbl = double :requester
+    time_error = TimeError.new(requester_dbl)
     from_time = Time.new(2022, 03, 14, 14, 10, 04)
     to_time = Time.new(2022, 03, 14, 16, 10, 04)
     allow(Time).to receive(:now).and_return(from_time)
     allow(requester_dbl).to receive(:get).with(URI("https://worldtimeapi.org/api/ip")).and_return('{"abbreviation":"GMT","client_ip":"2.25.179.217","datetime":"2022-03-14T16:41:36.214527+00:00","day_of_week":1,"day_of_year":73,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":0,"timezone":"Europe/London","unixtime":1647276096,"utc_datetime":"2022-03-14T16:41:36.214527+00:00","utc_offset":"+00:00","week_number":11}')
-    expect(time_error.error).to eq 9385.478979
+    expect(time_error.error).to eq 9092.214527
   end
 end
